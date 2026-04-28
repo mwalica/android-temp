0d446e59f2e27807cfd266ff75c9e9aea447008b
Marek Walica <mwalica@gmail.com>
2026-04-28 13:47:22 +0200

    init commit

---
+++ app/src/main/java/ch/walica/a77_temp280426_3tp1_wieleaktywnosci/MainActivity.java
@@ -0,0 +1,33 @@
+package ch.walica.a77_temp280426_3tp1_wieleaktywnosci;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    private EditText etEnteredText;
+    private Button btnToSecond;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        etEnteredText = findViewById(R.id.etEnteredText);
+        btnToSecond = findViewById(R.id.btnToSecond);
+
+    }
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,23 @@
+pluginManagement {
+    repositories {
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "77_temp280426_3tp1_wieleaktywnosci"
+include(":app")
